91009cc3ebeabca2a1b1821176dc382e93edf5f7
AtharNoor <atharnoor2@gmail.com>
2017-11-29 21:26:48 +0500

    TODO added in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json } do
+    #TODO show v1 in the url
     scope module: :v1 do
       resources :heroes, only: [:index, :show] do
         member do
