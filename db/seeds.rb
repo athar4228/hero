e2ffa02b6c157289e70654af731d1bd5a8017fba
AtharNoor <atharnoor2@gmail.com>
2017-11-29 21:26:39 +0500

    Seed: added hero and abilties

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "Creating Heros:"
+
+h = Hero.create!(
+  name: 'Tom Hardy',
+  real_name: 'Tom Hardy',
+  health: 100,
+  armour: 100,
+  shield: 100
+)
+
+Ability.create!(
+  hero_id: h.id,
+  name: 'Biotic Rifle',
+  description: 'Ana\u2019s rifle shoots darts that can restore health to her allies or deal ongoing damage to her enemies. She can use the rifle\u2019s scope to zoom in on targets and make highly accurate shots.',
+  is_ultimate: false,
+)
+
+Ability.create!(
+  hero_id: h.id,
+  name: 'Sleep Dart',
+  description: 'Ana fires a dart from her sidearm, rendering an enemy unconscious (though any damage will rouse them).',
+  is_ultimate: false,
+)
+
+
+h = Hero.create!(name: 'Adam', real_name: 'Adman', health: 100, armour: 100, shield: 100)
+
+Ability.create!(
+  hero_id: h.id,
+  name: 'Biotic Rifle',
+  description: 'Ana\u2019s rifle shoots darts that can restore health to her allies or deal ongoing damage to her enemies. She can use the rifle\u2019s scope to zoom in on targets and make highly accurate shots.',
+  is_ultimate: false,
+)
+
+Ability.create!(
+  hero_id: h.id,
+  name: 'Sleep Dart',
+  description: 'Ana fires a dart from her sidearm, rendering an enemy unconscious (though any damage will rouse them).',
+  is_ultimate: false,
+)
